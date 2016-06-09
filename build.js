cfcffd0bad744250f80bda8b952a5e3b1722c3c2
Nikos Katsikanis <nKatsikanis@gmail.com>
2016-06-09 15:10:07 +0100

    add build file

---
+++ build.js
@@ -0,0 +1,13 @@
+var stealTools = require('steal-tools');
+
+var promise = stealTools.build({
+  main: 'app/app.jsx',
+  config: __dirname + '/package.json!npm',
+}, {
+  // the following are the default values, so you don't need
+  // to write them.
+  minify: true,
+  debug: true,
+  bundleSteal: true,
+})
+
