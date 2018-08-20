009c629315f67ee07aa062209e2581a4812cdb85
Nikos Katsikanis <nkatsikanis@gmail.com>
2018-08-20 16:42:04 +0100

    Persist the state on re runs

---
+++ src/LoadingView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HelloWorld = () => (
+  <div>Loading</div>
+);
+
+export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import reducer from './reducers'
 import App from './App'
+import LoadingView from './LoadingView'
 
-const store = createStore(reducer)
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import {persistor, store} from './store';
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<LoadingView/>} persistor={persistor}>
+      <App/>
+    </PersistGate>
   </Provider>
   ,
   document.getElementById('root')
